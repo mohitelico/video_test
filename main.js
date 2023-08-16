540c08b42868e9614f5945034240bb406f0276c9
mtk171 <mtk17.1@gmail.com>
2023-08-16 11:57:06 +0530

    second

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const express=require('express')
+const cors=require('cors')
+const {v4:uuidV4}=require('uuid')
+const {PeerServer}=require('peer')
+const fs=require('fs')
+const path=require('path')
+const http=require('http')
+
+
 const app=express()
+app.use(cors())
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
 
 const port=process.env.PORT||3000
 
+const server=http.createServer(app)
+const io=require('socket.io')(server)
+
+
+
+
+
 app.get("/",(req,res)=>{
-    res.send("Welcome to test")
+    res.redirect(`/${uuidV4()}`)
 })
+
+app.get("/:room",(req,res)=>{
+    res.render('room',{roomId:req.params.room})
+})
+
 
 app.listen(port,()=>{
     console.log("server is ruuning")
